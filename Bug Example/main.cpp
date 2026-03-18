ab0f2c36e29ce48315781bef6b03193c647eaeae
Issa Ziada <issa000ziadeh@gmail.com>
2026-03-18 12:46:09 +0200

    Initial Commit

---
+++ Bug Example/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bug Example
+//
+//  Created by Issa's Mac on 18/03/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
