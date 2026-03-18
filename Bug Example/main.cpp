5c302c87a7b977e92166c6b5ec541838bd35fc86
Issa Ziada <issa000ziadeh@gmail.com>
2026-03-18 14:06:29 +0200

    sekeleton of caae 4

--- Bug Example/main.cpp
+++ Bug Example/main.cpp
@@ -59,6 +59,13 @@ int main() {
       case 3:
         displayArray(total, SIZE);
         break;
+      // All is positve
+        case 4:
+            if (isAllPositive(quantity, SIZE))
+                cout<< "\nAll elements of array is positive";
+            else
+                cout<<"/n At-least one of the elements are not positive";
+            break;
       // Exit
       case 0:
         // No code needed
@@ -86,6 +93,7 @@ int printMenu(){
     cout << "\n1) Enter quantity";
     cout << "\n2) Calculate total";
     cout << "\n3) Print total";
+    cout<<  "\4 All positive"
     cout << "\n0) Exit";
 
     cout << "\nEnter the choice: ";
